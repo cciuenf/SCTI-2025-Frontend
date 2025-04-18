05e29597235f10afe2f8e63cd38455773e11bd42
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2025-04-17 22:03:37 -0300

    feat: added cookies on dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,18 @@
-export default function Dashboard() {
+import { cookies } from "next/headers";
+
+export default async function Dashboard() {
+  const cookieStore = cookies() as ReturnType<typeof cookies>;
+  const access_token = (await cookieStore).get("access_token")?.value;
+  const refresh_token = (await cookieStore).get("refresh_token")?.value;
   return (
-    <div className="h-screen flex justify-center items-center font-mono">
-      <p>Dashboard</p>
+    <div className="h-screen flex flex-col items-center font-mono p-4">
+      <h1 className="font-black text-lg mb-3.5">Meus Tokens:</h1>
+      <p className="max-w-lvw p-2 break-words">
+        Access Token: {access_token ?? ""}
+      </p>
+      <p className="max-w-lvw p-2 break-words">
+        Refresh Token: {refresh_token ?? ""}
+      </p>
     </div>
   );
 }
