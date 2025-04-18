920447a1fea2464836995a40091745da7d353e24
juanzeen <jziignerdev@gmail.com>
2025-04-17 21:42:41 -0300

    feat: creating button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="h-screen flex justify-center items-center font-mono">
       <p>Home</p>
+      <Button>Primary</Button>
     </div>
   );
 }
