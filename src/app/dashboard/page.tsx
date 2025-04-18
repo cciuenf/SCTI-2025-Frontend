a2067c38d50baffd49c0216b665dc4229b016b41
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2025-04-18 10:36:45 -0300

    added jsonwebtoken to read the access token info

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,22 @@
 import { handleGetRefreshTokens } from "@/actions/auth-actions";
 import RefreshTokenList from "@/components/RefreshTokensList";
 import { cookies } from "next/headers";
+import jwt from 'jsonwebtoken';
+import { UserAccessTokenJwtPayload } from "@/types/authI";
 
 export default async function Dashboard() {
   const cookieStore = cookies() as ReturnType<typeof cookies>;
   const access_token = (await cookieStore).get("access_token")?.value;
+  const user_info = jwt.decode(access_token as string) as UserAccessTokenJwtPayload | null
   const refresh_token = (await cookieStore).get("refresh_token")?.value;
 
   const refreshTokens = await handleGetRefreshTokens();
   const tokens = refreshTokens[0];
   return (
     <div className="h-screen flex flex-col items-center font-mono p-4">
+      <h1 className="font-black text-lg mb-3.5">Nome: {user_info?.name} {user_info?.last_name}</h1>
+      <h1 className="font-black text-lg mb-3.5">Email: {user_info?.email}</h1>
+      <h1 className="font-black text-lg mb-3.5">Tipo de Admin: {user_info?.admin_type}</h1>
       <h1 className="font-black text-lg mb-3.5">Meus Tokens:</h1>
       <p className="max-w-lvw p-2 break-words">
         Access Token: {access_token ?? ""}
--- src/types/authI.ts
+++ src/types/authI.ts
@@ -9,3 +9,11 @@ export interface RefreshTokenI {
   DeletedAt: string;
   token_str: string;
 }
+
+export interface UserAccessTokenJwtPayload {
+  name: string;
+  last_name: string;
+  email: string;
+  admin_type: string;
+  // event: 
+}
