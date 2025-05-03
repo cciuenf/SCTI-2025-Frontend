cfc038a39b4e42b9c2e226daa763f27bd6df3e23
juanzeen <jziignerdev@gmail.com>
2025-05-03 03:11:55 -0300

    fix: small adjusts in the home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,15 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col gap-4 justify-center items-center font-mono w-1/2 mx-auto">
+    <div className="h-screen flex flex-col gap-4 justify-center items-center font-spartan w-1/2 mx-auto">
       <Image src={"/SCT.svg"} alt="Logo SCT" width={250} height={250} />
-      <h2>
-        Bem vindo ao site da SCT. Ainda estamos finalizando todos os detalhes
-        para te trazer a melhor experiência. Por hora, aproveite para criar a
-        sua conta ou realizar login!
+      <h2 className="text-foreground">
+        Bem vindo ao site da SCT.{" "}
+        <span className="text-secondary">
+           Ainda estamos finalizando todos os detalhes para te trazer a melhor
+          experiência. {" "}
+        </span>
+        Por hora, aproveite para criar a sua conta ou realizar login!
       </h2>
       <div className="flex w-full justify-around items-center">
         <Button>
