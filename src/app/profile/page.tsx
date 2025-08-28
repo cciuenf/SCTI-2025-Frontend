b8bbf806fb4f473585fb0427b946e352a61b0793
= <57465032+SoraAsc@users.noreply.github.com>
2025-08-28 14:45:38 -0300

    added a margin and increase the sidebar button size

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -59,7 +59,7 @@ const ProfilePage = async ({ searchParams }: Props) => {
   ]
 
   return (
-    <div className="flex flex-col items-center max-h-screen w-full overflow-hidden">
+    <div className="flex flex-col items-center max-h-screen w-full overflow-hidden mt-16 sm:mt-8">
       <CustomGenericTabs
         tabs={tabs}
         initialTabId={view}
