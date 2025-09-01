47229eb138672067a6950e0fc8ab6cd0f2423925
juanzeen <jziignerdev@gmail.com>
2025-09-01 00:45:56 -0300

    feat: card and social icons components, also added filipe and mateus

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import collaborators from "@/data/collaborators.json";
 import Connector from "@/components/ui/Generic/Connector";
-import { cn } from "@/lib/utils";
-import { Github, Linkedin } from "lucide-react";
+import CollabsCard from "@/components/Collabs/CollabsCard";
 
 const page = () => {
   return (
@@ -10,37 +9,25 @@ const page = () => {
       className="text-center flex flex-col items-center !mt-20"
       id="info"
     >
-      <div className="w-full max-w-6xl flex flex-col items-center gap-3">
+      <div className="w-full max-w-7xl flex flex-col items-center gap-3">
         <h1 className="font-bold text-6xl">Conheça a Nossa Equipe!</h1>
         <h2 className="font-light text-2xl text-center">
           Nossa equipe de colaboradores é formada inteiramente por estudantes de
           Ciência da Computação, que se dedicaram ao máximo para entregar a
           melhor semana acadêmica para todos os participantes.
         </h2>
-        <div
-          className={cn(
-            "flex justify-between items-center relative max-w-[420px] max-h-[320px] ",
-            "bg-[url(/collabs/yellow-figure.svg)]",
-            "bg-no-repeat bg-cover"
-          )}
-        >
-          <div className="flex flex-col w-1/2 h-[260px] justify-between overflow-hidden">
-            <div className="flex flex-col gap-1">
-              <h2 className="font-bold text-3xl text-secondary">{`${collaborators[0].last_name}, ${collaborators[0].name}`}</h2>
-              <h2 className="font-normal">{collaborators[0].function}</h2>
-            </div>
-            <div className="flex w-4/5 justify-center gap-2">
-              <Linkedin className="w-6 h-6"/>
-              <Github className="w-6 h-6"/>
-            </div>
-          </div>
-          <div className="w-[240px] h-[300px] overflow-hidden">
-            <img
-              src={`${collaborators[0].photo}`}
-              alt={`Foto de ${collaborators[0].name} ${collaborators[0].last_name}`}
-              className="w-full h-full object-cover"
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {collaborators.map((collab) => (
+            <CollabsCard
+              key={collab.name}
+              name={collab.name}
+              last_name={collab.last_name}
+              photo={collab.photo}
+              role={collab.role}
+              social_1={collab.social_1}
+              social_2={collab.social_2}
             />
-          </div>
+          ))}
         </div>
       </div>
     </Connector>
---
+++ src/components/Collabs/CollabsCard.tsx
@@ -0,0 +1,54 @@
+import { cn } from "@/lib/utils";
+import { Linkedin, Github } from "lucide-react";
+import React from "react";
+import SocialIcon from "./SocialIcon";
+
+type Props = {
+  name: string;
+  last_name: string;
+  photo: string;
+  role: string;
+  social_1: { icon: string; url: string };
+  social_2: { icon: string; url: string };
+};
+
+const CollabsCard = ({
+  role,
+  last_name,
+  name,
+  photo,
+  social_1,
+  social_2,
+}: Props) => {
+  return (
+    <div>
+      <div
+        className={cn(
+          "flex justify-between items-center relative max-w-[380px] max-h-[280px] ",
+          "bg-[url(/collabs/yellow-figure.svg)]",
+          "bg-no-repeat bg-cover"
+        )}
+      >
+        <div className="flex flex-col w-1/2 h-[220px] py-1 pl-8 justify-between">
+          <div className="flex flex-col gap-1">
+            <h2 className="font-bold text-3xl text-secondary">{`${last_name}, ${name}`}</h2>
+            <h2 className="font-normal">{role}</h2>
+          </div>
+          <div className="flex w-4/5 justify-center gap-2 z-50">
+            {<SocialIcon icon={social_1.icon} url={social_1.url} />}
+            {<SocialIcon icon={social_2.icon} url={social_2.url} />}
+          </div>
+        </div>
+        <div className="w-[200px] h-[260px] z-40 absolute -bottom-10 -right-5">
+          <img
+            src={photo}
+            alt={`Foto de ${name} ${last_name}`}
+            className="w-[160px] h-[220px]"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CollabsCard;
