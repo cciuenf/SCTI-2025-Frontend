dc8d8bbe8dbbd3fef7fd2dc1b5e012d036efc614
juanzeen <jziignerdev@gmail.com>
2025-09-01 01:04:44 -0300

    fix: vinicius linkedin and page text

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -10,11 +10,9 @@ const page = () => {
       id="info"
     >
       <div className="w-full max-w-7xl flex flex-col items-center gap-3">
-        <h1 className="font-bold text-6xl">Conheça a Nossa Equipe!</h1>
-        <h2 className="font-light text-2xl text-center">
-          Nossa equipe de colaboradores é formada inteiramente por estudantes de
-          Ciência da Computação, que se dedicaram ao máximo para entregar a
-          melhor semana acadêmica para todos os participantes.
+        <h1 className="font-bold text-5xl">Time SCTI 2025</h1>
+        <h2 className="font-light text-xl text-center">
+          Conheça a nossa equipe de colaboradores!
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {collaborators.map((collab) => (
